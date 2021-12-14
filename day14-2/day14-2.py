d4b271c879afe3e3feedc9ebd866ce4b468c2692
HelpfulHelper <github@digs.life>
2021-12-14 04:13:02 -0500

    better commenting?

--- day14-2/day14-2.py
+++ day14-2/day14-2.py
@@ -9,14 +9,21 @@ def gobble(filename):
 
 ###WAIT WE DON'T CARE ABOUT THE ACTUAL STRING - WE CARE ABOUT THE DIGRAPHS
 #split initial string into digraphs
-#if not a match, that digraph stays at 1
+#if not a match in our rules, that digraph will disappear
 #BUT for each match to our rules generates TWO digraphs - i[0] + new, and new + i[0]. add these to the counts
 #as such we can iterate over our curent set of counts for each digraph, and generate a new count for each new step
+
+
+
+
 def step(rules:dict, prev:Counter()):
+    #new is gonna be our counts of each digraph after the iteration
     new = Counter()
+    #for each digraph in our previous round
     for k in prev:
+        #check our rulebook
         if k in rules:
-            #print(rules[k])
+            #if digraph foo is in our rule book, counts of digraph xA and Ay += foo
             new[rules[k][0]] += prev[k]
             new[rules[k][1]] += prev[k]
     #print(prev, new)
@@ -24,12 +31,15 @@ def step(rules:dict, prev:Counter()):
 
 def main():
     data = gobble('input')
+    #start is our initial polymer state
     start = data[0][0]
+    #parse our set of rules once to generate dictionary of valid rules (xy = xAy)
     rules = dict([(i[0],i[2]) for i in [d for d in data[2:]]])
+    #simplify our rules - every match will give us xA and Ay as a digraph pairing.
     for k,v in rules.items():
         #print(k[0])
         rules[k] = k[0]+v, v+k[1]
-    
+    #convert our initial polymer state into a set of digraphs, and establish an initial count for each digraph
     digraphs = Counter(["".join(f) for f in zip(start, start[1:])])
 
     #step4 = "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
@@ -37,7 +47,7 @@ def main():
     #print(digraphs)
 
 
-    for x in range(40):
+    for x in range(4000):
         digraphs = step(rules, digraphs)
 
     tots = Counter()
